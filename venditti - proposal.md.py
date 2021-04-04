36abc1e6f3c8d436957e42be1d330e61144b2bd3
mvenditti99 <81570381+mvenditti99@users.noreply.github.com>
2021-04-03 22:11:32 -0400

    Add files via upload

---
+++ venditti - proposal.md.py
@@ -0,0 +1,16 @@
+# Preproposal
+
+## I will be composing a song using multiple different AI music softwares,
+such as Magenta Studio and Music Transformer
+
+## I am not collaborating with any classmates
+
+## To Do List
+- compose motif by editing project magenta code
+- use music transformer to elaborate on motif
+- use project magenta code to create other instruments possibly, such as
+drums, etc
+- compose full length song in fl studio using ai generated motif and coded instruments
+- I can also add more instruments from midi with magenta tone transformer
+
+##No Questions, I'm just curious if this sounds like enough work.
