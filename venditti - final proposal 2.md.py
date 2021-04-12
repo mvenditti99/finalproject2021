a8055aa0c35de871a1da09b4786ec6021981002d
mvenditti99 <81570381+mvenditti99@users.noreply.github.com>
2021-04-12 00:42:36 -0400

    Add files via upload

---
+++ venditti - final proposal 2.md.py
@@ -0,0 +1,49 @@
+# Proposal
+
+## What will (likely) be the title of your project?
+
+Composing a Song Using Project Magenta and Randomization Elements
+
+## In just a sentence or two, summarize your project. (E.g., "A website that lets you buy and sell stocks.")
+
+I will create a melody and other intstrument tracks using Project Magenta. I will import that into FL Studio where I will create a finished song. 
+
+## In a paragraph or more, detail your project. What will your software do? What features will it have? How will it be executed?
+
+I will first edit the code for intruments/melodies in Project Magenta to creat a motif that I will be able to work around. Then, I will use Nativer Plugin's Music
+Transformer to create an expended motif, or even a whole melody. That allows me to be able to do coding and to get experience using randomized softwares. After that,
+I will create a few more intruments/and sounds using Project Magenta that I can incorporate into the final song. Lastly, I will use FL studio and build a song around that
+randomized motif, using the extra intruments I have created through coding, alongside with other intrument libraries that I already own. If I have extra time, I do
+want to try to incorporate more randomized elements in the song using plugins within FL Studio.
+
+## If planning to combine 1051's final project with another course's final project, with which other course? And which aspect(s) of your proposed project would relate to 1051, and which aspect(s) would relate to the other course?
+
+N/A
+
+## If planning to collaborate with 1 or 2 classmates for the final project, list their names, email addresses, and the names of their assigned TAs below.
+
+N/A, I am working alone. My Temple email is tug92646@temple.edu.
+
+## In the world of software, most everything takes longer to implement than you expect. And so it's not uncommon to accomplish less in a fixed amount of time than you hope.
+
+### In a sentence (or list of features), define a GOOD outcome for your final project. I.e., what WILL you accomplish no matter what?
+
+A motif generated through coding, use of Music Transformer to randomize the motif, a completed song in FL Studio using those elements.
+
+### In a sentence (or list of features), define a BETTER outcome for your final project. I.e., what do you THINK you can accomplish before the final project's deadline?
+
+A motif generated through coding, use of Music Transformer to randomize the motif, other extra intruments and melodies generated through coding in Project Magenta,
+a completed song in FL Studio using those elements.
+
+### In a sentence (or list of features), define a BEST outcome for your final project. I.e., what do you HOPE to accomplish before the final project's deadline?
+
+A motif generated through coding, use of Music Transformer to randomize the motif, other extra intruments and melodies generated through coding in Project Magenta,
+use of plugins in FL studio that randomize other aspects of the song, a completed song in FL Studio using those elements.
+
+## In a paragraph or more, outline your next steps. What new skills will you need to acquire? What topics will you need to research? If working with one of two classmates, who will do what?
+
+I chose this project because while I am a Music Technology major and am already comfortable in creating music under time constraints, I have yet to create a song using
+elements of coding. I'm confident that I  will be able to create a successful motif using the code within Project Magenta, but it is definitely something I have to
+study a bit more so that things can sound the way I want them to. I have also had plenty of experience using randomization in music, but nothing like this, where
+the melody/motif of the entire song is subjected to change on however the software wants it to. I don't think that I have many new technical skills that I need to learn
+for this project besides being able to produce notes/different tones in code, but I am a bit nervous about having to create something around whatever the computer gives me!
