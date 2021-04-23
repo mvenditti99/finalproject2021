cb4c19cab3bb7d608561cfd6f1d4fd8c02e653da
mvenditti99 <81570381+mvenditti99@users.noreply.github.com>
2021-04-22 23:21:26 -0400

    Add files via upload

---
+++ venditti - status.md.py
@@ -0,0 +1,30 @@
+# Status Report
+
+#### Your name
+
+Meghan Venditti
+
+#### Project title
+
+Creating a Song using Coding, Randomization, and Project Magenta
+
+***
+
+Short answers for the below questions suffice. If you want to alter your plan for your project (and obtain approval for the same), be sure to email your section leader directly!
+
+#### What have you done for your project so far?
+I was able to code the main tune and drum beat from editing Project Magenta's code and saving them as audio files. I then used a program called Piano-Scribe, which uses ai to turn the audio
+files of the main tune (motif) and drum beat into midi files. The drum beat midi needed a bit of editing in FL Studio, since it was percussion and not piano. 
+After that, I was able too extend the motif into a longer melody using 'Continue' by Project Magenta. I was also able to extend drums the with 'Groove' and 'Continue'
+in Project Magenta. 
+
+
+#### What have you not done for your project yet?
+I still have to create other instrument parts in midi, and I really want to experiment more with the randomization elements of Project Magenta, since it is AI based, and maybe another software.
+After I write the rest of the song parts based off of the melody I originally coded and finish randomizing elements, I just need to polish up and put together the song!
+
+TODO
+
+#### What problems, if any, have you encountered?
+I was originally planning to save the midi right from the project magenta code, but it does not let me do that. I have to save my edited code as an audio file, and then
+use a really cool program called Piano scribe that use AI to turn the sudio file into MIDI for me to edit in the song. 
